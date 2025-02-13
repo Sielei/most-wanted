51d3c9b55afcf91e2d01f7cbcc37114302882923
Sielei Herman <hsielei@gmail.com>
2025-02-14 02:24:53 +0300

    improvement: configure resilience4j rate limiter

--- backend/src/main/java/com/hs/backend/common/ApplicationConfig.java
+++ backend/src/main/java/com/hs/backend/common/ApplicationConfig.java
@@ -1,7 +1,10 @@
-package com.hs.backend.config;
+package com.hs.backend.common;
 
 import com.hs.backend.wanted.MostWanted;
 import com.hs.backend.wanted.MostWantedList;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.spring6.ratelimiter.configure.RateLimiterAspect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+import java.util.function.Supplier;
 
 @Configuration
 public class ApplicationConfig {
@@ -46,4 +52,19 @@ public class ApplicationConfig {
 
         return template;
     }
+
+    @Bean
+    public RateLimiter rateLimiter (){
+        var rateLimiterConfig = RateLimiterConfig.custom()
+                .limitForPeriod(10)
+                .limitRefreshPeriod(Duration.ofSeconds(1))
+                .timeoutDuration(Duration.ofSeconds(10))
+                .build();
+        return RateLimiter.of("fbiMostWanted", rateLimiterConfig);
+    }
+
+//    @Bean
+//    public RateLimiterAspect rateLimiterAspect() {
+//        return new RateLimiterAspect();
+//    }
 }
