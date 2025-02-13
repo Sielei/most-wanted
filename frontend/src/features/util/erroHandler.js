e596c11b73f5de840448a59c2eb25fec94c8dd4f
Sielei Herman <hsielei@gmail.com>
2025-02-14 02:25:46 +0300

    improvement: create a utility class to handle errors and display error messages on toast notifications

---
+++ frontend/src/features/util/erroHandler.js
@@ -0,0 +1,15 @@
+import {toast} from "react-toastify";
+
+export const handleApiError = (error) => {
+    const errorMessage = error.response?.data?.message || error.message ||
+        'An unexpected error occurred';
+
+    toast.error(errorMessage, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+    });
+};
