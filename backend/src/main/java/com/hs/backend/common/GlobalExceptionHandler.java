09eb9e36690e7535090adb20d1c7e22985076309
Sielei Herman <hsielei@gmail.com>
2025-02-14 11:55:48 +0300

    fix: update exception handler to fix conflict with springdocs

--- backend/src/main/java/com/hs/backend/common/GlobalExceptionHandler.java
+++ backend/src/main/java/com/hs/backend/common/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.hs.backend.common;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,6 +16,7 @@ import java.util.Locale;
 import java.util.concurrent.TimeoutException;
 
 @RestControllerAdvice
+@Hidden
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
@@ -21,8 +24,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = {RestClientException.class, TimeoutException.class})
     ProblemDetail handleExternalAPICallExceptions(Exception exception) {
         log.error(exception.getMessage(), exception);
+        var location = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/v1/errors/internal-server-error")
+                .build().toUri();
         var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
         problemDetail.setTitle("Server Error");
+        problemDetail.setType(location);
         problemDetail.setProperty("timestamp", DateTimeFormatter
                 .ofPattern("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
                 .format(LocalDateTime.now()));
